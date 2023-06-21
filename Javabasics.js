6f99c94f7f3157da9d25750506a5b6894b55a548
Berodtm <134790573+Berodtm@users.noreply.github.com>
2023-06-21 22:11:28 +0100

    Add files via upload

---
+++ Javabasics.js
@@ -0,0 +1,13 @@
+let button = document.querySelector('button');
+let box = document.getElementById('changeMe');
+
+button.onclick = function changeColor() {
+    if (box.style.background == 'red'){
+    box.style.background = 'blue';
+    } else {
+        box.style.background = 'red';
+    }
+    
+
+}
+
